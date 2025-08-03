19ec5274f3f4b4b9b641dbda3bea208db365855d
Your Name <victor.lopesvfk11@gmail.com>
2025-08-03 11:31:23 -0300

    add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ export default function Hero() {
         <div className='w-full'>
           <h1 className='font-Golos font-extrabold text-4xl text-white lg:text-6xl'>Victor Brasileiro</h1>
           <h3 className='font-Golos text-gray-400 font-medium lg:text-xl'>Desenvolvedor .NET & Django</h3>
-          <p className='mt-3 font-Golos text-gray-300 font-medium max-w-[92%] 2xl:text-[18px]'>Desenvolvo backends robustos e escaláveis, criando APIs eficientes e sistemas seguros com <span className='font-extrabold'>.NET e C#.</span> Minha missão é transformar dados em soluções ágeis e confiáveis para seu projeto.</p>
+          <p className='mt-3 font-Golos text-gray-300 font-medium max-w-[92%] 2xl:text-[18px]'>Desenvolvo backends robustos e escaláveis, criando APIs eficientes e sistemas seguros com <span className='font-extrabold'>.NET, C#. e Django</span> Minha missão é transformar dados em soluções ágeis e confiáveis para seu projeto.</p>
         </div>
 
         {/* categories */}
